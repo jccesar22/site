a78abac4b4810c80ed072e8839caffdfc6b0b5f6
jccesar22 <60848753+jccesar22@users.noreply.github.com>
2021-02-10 18:58:01 -0300

    commit

--- Script.js
+++ Script.js
@@ -1,10 +1,10 @@
 let box = document.getElementById('box') 
 function carregar(){
-   if (box.checked){
+   if (box.checked == true){
       document.body.style.background = 'black'
       document.body.style.color = 'white'
       document.body.style.footer = 'white'
-   }else{
+   }else if (box.checked == false){   
       document.body.style.background = 'white'
       document.body.style.color = 'black'
       document.body.style.footer = 'black' 
